0afa75c68b2cf20f8817e5388d019d3dc8d0b6c0
Administrador <Administrador@pc01>
2013-04-22 01:23:46 -0500

    commit2

--- tets/src/test.java
+++ tets/src/test.java
@@ -5,6 +5,8 @@ public class test {
 	 * @param args
 	 */
 	public test() {
+		int number = 15;
+		String hola = "Hola";
 	}
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
